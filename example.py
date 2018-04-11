a4b9061d0219f416cb703c98b5232144acba8327
byue <yue_bryan123@hotmail.com>
2018-04-11 13:55:45 -0700

    modified example to save files

--- example.py
+++ example.py
@@ -2,6 +2,8 @@ import collections
 import contextlib
 import sys
 import wave
+import os
+from os import path
 
 import webrtcvad
 
@@ -98,7 +100,7 @@ def vad_collector(sample_rate, frame_duration_ms,
     for frame in frames:
         is_speech = vad.is_speech(frame.bytes, sample_rate)
 
-        sys.stdout.write('1' if is_speech else '0')
+        #sys.stdout.write('1' if is_speech else '0')
         if not triggered:
             ring_buffer.append((frame, is_speech))
             num_voiced = len([f for f, speech in ring_buffer if speech])
@@ -107,7 +109,7 @@ def vad_collector(sample_rate, frame_duration_ms,
             # TRIGGERED state.
             if num_voiced > 0.9 * ring_buffer.maxlen:
                 triggered = True
-                sys.stdout.write('+(%s)' % (ring_buffer[0][0].timestamp,))
+                #sys.stdout.write('+(%s)' % (ring_buffer[0][0].timestamp,))
                 # We want to yield all the audio we see from now until
                 # we are NOTTRIGGERED, but we have to start with the
                 # audio that's already in the ring buffer.
@@ -124,35 +126,52 @@ def vad_collector(sample_rate, frame_duration_ms,
             # unvoiced, then enter NOTTRIGGERED and yield whatever
             # audio we've collected.
             if num_unvoiced > 0.9 * ring_buffer.maxlen:
-                sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
+                #sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
                 triggered = False
                 yield b''.join([f.bytes for f in voiced_frames])
                 ring_buffer.clear()
                 voiced_frames = []
-    if triggered:
-        sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
-    sys.stdout.write('\n')
+    #if triggered:
+        #sys.stdout.write('-(%s)' % (frame.timestamp + frame.duration))
+    #sys.stdout.write('\n')
     # If we have any leftover voiced audio when we run out of input,
     # yield it.
     if voiced_frames:
         yield b''.join([f.bytes for f in voiced_frames])
 
-
+# replaces source wav file with chopped up wave files with silence removed
+# returns number of frames at end. returns 1 if file was not removed or chopped.
+# returns 1 if file was shortened to 1 output file. returns number
+# of final files with or without replacement
 def main(args):
     if len(args) != 2:
         sys.stderr.write(
             'Usage: example.py <aggressiveness> <path to wav file>\n')
         sys.exit(1)
-    audio, sample_rate = read_wave(args[1])
-    vad = webrtcvad.Vad(int(args[0]))
+    input_path = args[1]
+    aggressiveness = int(args[0])
+    audio, sample_rate = read_wave(input_path)
+    directory = os.path.dirname(input_path)
+    base = os.path.basename(input_path)
+    name = os.path.splitext(base)[0]
+    ext = os.path.splitext(base)[1]
+    vad = webrtcvad.Vad(aggressiveness)
     frames = frame_generator(30, audio, sample_rate)
     frames = list(frames)
     segments = vad_collector(sample_rate, 30, 300, vad, frames)
-    for i, segment in enumerate(segments):
-        path = 'chunk-%002d.wav' % (i,)
-        print(' Writing %s' % (path,))
+    counter = 0
+    curr_num = int(name.split('-')[1])
+    prefix = name.split('-')[0]
+    for segment in segments:
+        if (counter == 0):
+            os.remove(input_path)
+        path = os.path.join(directory, prefix + '-' + str(curr_num) + ext)
         write_wave(path, segment, sample_rate)
-
+        counter += 1
+        curr_num += 1
+    if (counter == 0):
+        return 1
+    return counter
 
 if __name__ == '__main__':
     main(sys.argv[1:])
